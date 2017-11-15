b465d766c724e962431bc5aba14965e1f996d48f
jacckjacck <jorge.gomez@tumesadeayuda.com>
2017-11-15 15:37:43 -0500

    adding unit test

--- server.js
+++ server.js
@@ -42,3 +42,5 @@ app.get('/', (req, res)=>{
 app.listen(port,() =>{
     console.log("Live on %s", port);
 })
+
+module.exports = app;
---
+++ tests/User/first-test.js
@@ -0,0 +1,35 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var expect = chai.expect;
+var userService = require('../../services/userService');
+var server = require('../../server');
+var shourl = chai.should();
+
+chai.use(chaiHttp);
+
+describe('/GET Users', () => {
+    var token = null;
+
+    before((done) => {
+        chai.request(server)
+        .post('/auth')
+        .send({username:'jagomez', password:'J0rg310', cedula:1})
+        .end((err,res) => {
+            token = res.body.token;
+            
+            done();
+        });
+    });
+
+
+    it('Get Method should return a list of users', (done) => {
+        chai.request(server)
+        .get('/users')
+        .set('authorization', 'Bearer '+token)
+        .end((err, res) => {
+            res.should.to.have.status(200);
+            res.body.should.to.be.a('array');
+            done();
+        });
+    })
+});
